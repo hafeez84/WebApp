0409d622fd8caccb7315ddcdd64ec0348ef87402
hafeez84 <h.qasemy@live.com>
2018-08-08 14:11:32 +0300

    created users table, controllers and views

--- WebApp/App_Start/RouteConfig.cs
+++ WebApp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Users", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
